9167ffdd59fdd177ea6252eb8f2a229a1600dcbd
anniou toybou <anniou.toybou@gmail.com>
2020-04-09 02:28:26 +0200

    Add link - New routes - Add new style show.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'gnoumbaas/index'
-  get 'gnoumbaas/show'
-  get 'gnoumbaas/create'
-  get 'gnoumbaas/update'
-  get 'dashboard/show'
-  get 'pages/home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   # gnoumbaa actions
-  resources :gnoumbaa, only: [:index, :show, :new, :create, :update]
+  resources :gnoumbaas, only: [:index, :show, :new, :create, :update]
   # dashboard user
   resource :dashboard, only: [:show]
 end
